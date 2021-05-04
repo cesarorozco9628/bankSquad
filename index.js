9fc36e8ba01e00eca186f273af3a826e74f2e13f
Cesar Orozco <paulocesarorozcoguerrero@hotmail.com>
2021-05-04 14:47:33 -0500

    CES

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 const {AccountTypes, Clients} = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname + 'public')));
 app.set('view engine', 'ejs');
 
 
